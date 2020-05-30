1740ebb7dcebdbe7fae3a8082f3b2a3a5a0bee11
Konstantin <balashov-92@mail.ru>
2020-05-30 16:20:36 +0300

    create database proxy

--- telegram_bot/db_inizialization.py
+++ telegram_bot/db_inizialization.py
@@ -1,20 +1,7 @@
 import peewee
-from playhouse.sqlite_ext import JSONField
 
 database_proxy = peewee.DatabaseProxy()
 
-
-class BaseTable(peewee.Model):
-    class Meta:
-        database = database_proxy
-
-
-class UserState(BaseTable):
-    """Состояние пользователя внутри сценария."""
-    user_id = peewee.CharField(unique=True)
-    scenario_name = peewee.CharField
-    step_name = peewee.CharField
-    context = JSONField()
 
 
 
