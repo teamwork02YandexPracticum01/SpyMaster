edc5705ad811c1e832c00f25e42e7067099fe15c
Konstantin <balashov-92@mail.ru>
2020-06-01 14:57:36 +0300

    Добавлена модель пользователя. Теперь модель отвечающая за состояние юзера внутри сценария называется CommandHandler

--- telegram_bot/models.py
+++ telegram_bot/models.py
@@ -5,13 +5,22 @@ from telegram_bot.db_inizialization import database_proxy
 
 
 class BaseTable(peewee.Model):
+    """Таблица от которой будут наследованы все модели."""
+
     class Meta:
         database = database_proxy
 
 
-class UserState(BaseTable):
+class CommandHandler(BaseTable):
     """Состояние пользователя внутри сценария."""
     user_id = peewee.CharField(unique=True)
     scenario_name = peewee.CharField
     step_name = peewee.CharField
     context = JSONField()
+
+
+class User(BaseTable):
+    """Цели для слежки для каждого user"""
+    user_id = peewee.CharField(unique=True)
+    max_targets = peewee.IntegerField
+    targets = peewee.ValuesList
